48a8d2dd9ebb424f3b60dc00cf8ab060cf5c5380
Maria Pavlenko <pavlenkompq@gmail.com>
2022-05-29 15:08:07 +0300

    Homework 3

---
+++ yatube/posts/models.py
@@ -0,0 +1,47 @@
+# posts/models.py
+from tokenize import group
+from django.db import models
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='group_posts'
+    )
+
+class Meta:
+    ordering = [('-pub_date')[:10], 'author']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
